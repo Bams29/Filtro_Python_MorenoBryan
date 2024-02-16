e8dafb65bb47c4da308ab43b25210c17c41d0e18
Bams29 <sarmientobryan2911@gmail.com>
2024-02-16 09:15:30 -0500

    .

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 
 
-with open("clientes.json", "r") as file:
-    clientes = json.load(file)
+with open("clientes.json", "r") as outfile:
+    clientes = json.load(outfile)
     
-
+nuevo_cliente = {}
 
 name = input("ingrese el nombre del nuevo cliente: ")
 apellido = input("ingrese el apellido del nuevo cliente: ")
@@ -15,15 +15,6 @@ direccion = input("ingrese la direccion del nuevo cliente: ")
 estrato = input("ingresa el estrato del nuevo cliente: ")
 
 
-nuevo_cliente = {
-    'nombre': name,
-    'apellido': apellido,
-    'edad': edad,
-    'dni': dni,
-    'telefono': telefono,
-    'direccion':direccion }
-
-clientes['nuevo'] = nuevo_cliente
 
 with open("clientes.json", "w") as file:
     json.dump(nuevo_cliente, file, indent=4)
