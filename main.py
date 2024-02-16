c16f166487cb2d7c2db7eb5644e0c60fc762597f
Bams29 <sarmientobryan2911@gmail.com>
2024-02-16 08:40:58 -0500

    d

--- main.py
+++ main.py
@@ -3,24 +3,37 @@ import json
 
 with open("clientes.json", "r") as file:
     clientes = json.load(file)
+    
+
+
+name = input("ingrese el nombre del nuevo cliente: ")
+apellido = input("ingrese el apellido del nuevo cliente: ")
+edad = input("ingrese la edad del nuevo usuario: ")
+dni = input("ingresa el nuemero de identificacion del usuario: ")
+telefono = input("ingrese el telefono del nuevo cliente: ")
+direccion = input("ingrese la direccion del nuevo cliente: ")
+estrato = input("ingresa el estrato del nuevo cliente: ")
+
+
+nuevo_cliencte = {
+    'nombre': name,
+    'apellido': apellido,
+    'edad': edad,
+    'dni': dni,
+    'telefono': telefono,
+    'direccion':direccion }
+
+with open("clientes.json", "w") as file:
+    json.dump(clientes, file, indent=4)
+
 
 #----------nuevo cliente------
 
-    nuevo_cliente = {}
 
-    clientes['nombre'] = input("ingrese el nombre del nuevo cliente: ")
-    clientes['apellido'] = input("ingrese el apellido del nuevo cliente: ")
-    clientes['edad'] = input("ingrese la edad del nuevo usuario: ")
-    clientes['dni'] = input("ingresa el nuemero de identificacion del usuario: ")
-    clientes['telefono'] = input("ingrese el telefono del nuevo cliente: ")
-    clientes['direccion'] = input("ingrese la direccion del nuevo cliente: ")
-    clientes['estrato'] = input("ingresa el estrato del nuevo cliente: ")
-    clientes['categoria'] = "cliente nuevo"
-    clientes['años afiliado'] = 0
-
-with open("clientes.json", "w") as file:
-    clientes = json.load(file)
     
+
+
+
 
 
 
